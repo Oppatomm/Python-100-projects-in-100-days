05c56d5f31b050c770d926eb4f86acd61ff53ee8
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2024-10-19 23:29:12 +0700

    Update Day 3

---
+++ Day3BMI calculation program.py
@@ -0,0 +1,14 @@
+height = int(input("Height : "))
+weight = int(input("Weight : "))
+cm_convert_m = height / 100
+height_multiply_height = cm_convert_m * cm_convert_m
+bmi = weight / height_multiply_height
+bmi = round(bmi,2)
+if bmi > 30 : 
+    print(str(bmi) + " : Obesity")
+elif bmi <= 29.9 and bmi >= 25 :
+    print(str(bmi) + " : Overweight")
+elif bmi <= 24.9 and bmi >= 18.5 :
+    print(str(bmi) + " : Normal weight")
+else :
+    print(str(bmi) + " : Underweight")
