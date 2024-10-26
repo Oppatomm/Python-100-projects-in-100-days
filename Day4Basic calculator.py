2f614aac383c0534d7f2b4845c74cf504e8d8e89
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2024-10-26 22:50:08 +0700

    Update Day 4

---
+++ Day4Basic calculator.py
@@ -0,0 +1,24 @@
+choose =  int(input("Please choose your choice.\n"
+                    "1 means addition (+)\n"
+                    "2 means subtraction (-)\n"
+                    "3 means multiplication (*)\n"
+                    "4 means division (/)\n"
+                    "Example Input : 1\n")
+            )
+number1 = int(input("Number 1 : "))
+number2 = int(input("Number 2 : "))
+
+match choose :
+    case 1 :
+        print(f"{number1} + {number2} = {number1 + number2}")
+    case 2 :
+        print(f"{number1} - {number2} = {number1 - number2}")
+    case 3 :
+        print(f"{number1} * {number2} = {number1 * number2}")
+    case 4 :
+        if number2 != 0 :
+            print(f"{number1} / {number2} = {number1 / number2}")
+        else :
+            print("Cannot divide by zero.")
+    case _ :
+        print("You entered incorrectly Please try again.")
